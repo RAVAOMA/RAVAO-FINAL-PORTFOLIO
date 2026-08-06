48db8547d56fb1c0a91b45ba8e9329b2348f63af
Mark <macbook@Macbooks-MacBook-Air-2.local>
2026-08-07 01:52:07 +0800

    Add function-based create views for projects inquiries and testimonies

--- website/views.py
+++ website/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from .models import PersonalInformation, Project
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import ProjectForm, InquiryForm, TestimonyForm
 
 def home(request):
     personal = PersonalInformation.objects.first()
@@ -24,3 +23,59 @@ def project_detail(request, pk):
     return render(request, "website/project_detail.html", {
         "project": project,
     })
+
+def project_create(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("project_list")
+    else:
+        form = ProjectForm()
+
+    return render(
+        request,
+        "website/project_form.html",
+        {"form": form},
+    )
+
+
+def inquiry_create(request):
+    if request.method == "POST":
+        form = InquiryForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("inquiry_success")
+    else:
+        form = InquiryForm()
+
+    return render(
+        request,
+        "website/inquiry_form.html",
+        {"form": form},
+    )
+
+
+def inquiry_success(request):
+    return render(request, "website/inquiry_success.html")
+
+
+def testimony_create(request):
+    if request.method == "POST":
+        form = TestimonyForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("testimony_list")
+    else:
+        form = TestimonyForm()
+
+    return render(
+        request,
+        "website/testimony_form.html",
+        {"form": form},
+    )
+
+
